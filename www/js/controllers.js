0fde2fb848078dc0a30693db1a1d9248c21e9b68
huaruiwu <huarui.wu@gmail.com>
2014-10-05 23:44:53 -0700

    add login window and send on submit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 'use strict'
 angular.module('starter')
-.controller('ChatController', ['$scope', '$firebase', 'fbUrl', function($scope, $firebase, fbUrl) {
+.controller('ChatController', ['$scope', '$firebase', 'fbUrl', '$ionicModal', function($scope, $firebase, fbUrl, $ionicModal) {
   var ref = new Firebase(fbUrl);
   var sync = $firebase(ref);
 
@@ -14,5 +14,29 @@ angular.module('starter')
   $scope.sendMessage = function() {
     chatList.$add({username: 'guest', message: $scope.currentMessage});
   };
+
+
+  // show login modal
+  $ionicModal.fromTemplateUrl('login.html', {
+    scope: $scope,
+    animation: 'slide-in-up',
+    backdropClickToClose: false,
+    hardwareBackButtonClose: false
+  }).then(function(modal) {
+    modal.show();
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  $scope.$on('modal.hidden', function() {
+  });
+  $scope.$on('modal.removed', function() {
+  });
 
 }]);
