e44b9a1282f2bc7c12b54723cce2bd86888eb1a6
huaruiwu <huarui.wu@gmail.com>
2014-10-06 01:04:47 -0700

    init

---
+++ js/controllers.js
@@ -0,0 +1,50 @@
+'use strict'
+angular.module('starter')
+.controller('ChatController', ['$scope', '$firebase', 'fbUrl', '$ionicModal', '$ionicScrollDelegate',function($scope, $firebase, fbUrl, $ionicModal, $ionicScrollDelegate) {
+  var ref = new Firebase(fbUrl);
+  var sync = $firebase(ref.endAt().limit(20));
+
+  var chatList = sync.$asArray();
+
+  $scope.currentUser = {userColor: randomColor({luminosity: 'dark'}), username: '', message: ''};
+
+  $scope.chatList = chatList;
+
+  $scope.sendMessage = function() {
+    chatList.$add($scope.currentUser);
+    $scope.currentUser.message = '';
+  };
+
+  $scope.$watch('chatList', function(val) {
+    $ionicScrollDelegate.resize();
+    $ionicScrollDelegate.scrollBottom();
+  }, true);
+
+
+  // show login modal
+  $ionicModal.fromTemplateUrl('login.html', {
+    scope: $scope,
+    animation: 'slide-in-up',
+    backdropClickToClose: false,
+    hardwareBackButtonClose: false
+  }).then(function(modal) {
+    modal.show();
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    if ($scope.currentUser.username.length > 0) {
+      $scope.modal.hide();
+    }
+  };
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  $scope.$on('modal.hidden', function() {
+  });
+  $scope.$on('modal.removed', function() {
+  });
+
+}]);
